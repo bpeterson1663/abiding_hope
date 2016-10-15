f2716cfd79141b5a448cb15b77cb9e6ac26bc579
bpeterson1663 <bpeterson1663@gmail.com>
2016-10-14 21:24:35 -0500

    Styling and formating changes

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <!--<meta name="viewport" content="width=device-width, initial-scale=1">-->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <meta name="Author" contnet="">
-        <meta name="Description" content="">
-        <meta name="Keywords" content="">
+        <meta name="Author" contnet="Brady Peterson">
+        <meta name="Description" content="There are the times in our lives when hope is hard to find, when we often have more questions than answers, and when fear and worry clamor to be our constant companions. Abiding Hope offers encouragement and inspiration for those times. ">
+        <meta name="Keywords" content="Abiding Hope, Encouragement, Self-Help Book, Inspirational, Religion & Spirituality, Worship and Devotion">
         <meta name="Robots" content="all">
 
         <!--<link rel="icon" type="image/png" href="">-->
@@ -39,7 +39,7 @@
             </div>    
             <div class="row">
                 <div class="col-md-12 col-md-offset-4 social-media">
-                    <a href="mailto:t@windahl.com"><img alt="Email" src="assets/images/email.png" /></a>
+                    <a href="mailto:t@twindahl.com"><img alt="Email" src="assets/images/email.png" /></a>
                     <a href="https://www.facebook.com/twindahlauthor" target="_blank"><img alt="Facebook" src="assets/images/facebook.png" /></a>
                     <a href="https://www.instagram.com/t.windahl/" target="_blank"><img alt="Instagram" src="assets/images/instagram.png" /></a>
                 </div>            
