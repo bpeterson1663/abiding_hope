88db08941bbee758d77ade09ee6f91d046da7f97
bpeterson1663 <bpeterson1663@gmail.com>
2016-10-14 19:43:02 -0500

    Initial Commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <!--<meta name="viewport" content="width=device-width, initial-scale=1">-->
+
+        <meta name="Author" contnet="">
+        <meta name="Description" content="">
+        <meta name="Keywords" content="">
+        <meta name="Robots" content="all">
+
+        <!--<link rel="icon" type="image/png" href="">-->
+
+        <title>Abiding Hope</title>
+        
+        <script src="vendors/jquery/dist/jquery.min.js" type="text/javascript"></script>
+        <script src="vendors/bootstrap/dist/js/bootstrap.min.js" type='text/javascript'></script>
+
+        <link href="vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet"/>
+
+        <link href="assets/styles/styles.css" rel="stylesheet" />
+        <script type="text/javascript" src="assets/scripts/app.js"></script>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row top-row">
+
+                    <img src="assets/images/author.png" class="img-responsive img author" alt="T. Windahl - author. speaker." />
+              
+                    <a href="https://www.amazon.com/Abiding-Hope-T-Windahl/dp/1939881102/ref=sr_1_1?ie=UTF8&qid=1476212839&sr=8-1&keywords=abiding+hope+t+windahl" target="_blank"><img alt="Buy Now" class="img-responsive buy-now" src="assets/images/buy_now.png" /></a>
+  
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <img alt="Abiding Hope" class="img-responsive" src="assets/images/hero_image.png" />
+                    <img alt="Encouragement and inspiration for hard times." class="img-responsive img" src="assets/images/summary.png"/>  
+                </div>
+            </div>    
+            <div class="row">
+                <div class="col-md-12 col-md-offset-4 social-media">
+                    <a href="mailto:t@windahl.com"><img alt="Email" src="assets/images/email.png" /></a>
+                    <a href="https://www.facebook.com/twindahlauthor" target="_blank"><img alt="Facebook" src="assets/images/facebook.png" /></a>
+                    <a href="https://www.instagram.com/t.windahl/" target="_blank"><img alt="Instagram" src="assets/images/instagram.png" /></a>
+                </div>            
+            </div>
+        </div>    
+    </body>    
+</html>
